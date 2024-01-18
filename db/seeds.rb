87168cc338dec3e97732fd3d93a2f6bde40ff626
HenaryKhuraijam <henarykhu123@gmail.com>
2024-01-18 16:51:35 +0530

    Add db:seed

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+  greetings = Greeting.create(
+    [
+        { message: "Khurumjari" },
+        { message: "Namaste" },
+        { message: "Guten Tag" },
+        { message: "Welcome" },
+        { message: "Greeting" }
+    ]
+    )
+
+
